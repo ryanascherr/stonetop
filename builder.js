15dce5109056f7d20602f796b81eb83248a5b913
ryanascherr <ryanascherr@gmail.com>
2021-04-19 00:56:32 -0400

    Changed purpose of page to be a character creator. Wrote HTML, CSS and JS for selecting a class. Added images of each class' icon.

---
+++ builder.js
@@ -0,0 +1,6 @@
+const createCharacterBtn = $("#create-character-btn");
+const selectCharacterBtn = $("#select-character-btn");
+
+createCharacterBtn.click(function(){
+    $("#class-selection-container").removeClass("hidden");
+})
