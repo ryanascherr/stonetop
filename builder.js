e8e628a89653bb73fc6c2cf324c0a058de2929ef
ryanascherr <ryanascherr@gmail.com>
2021-04-19 01:28:21 -0400

    Added background page.

--- builder.js
+++ builder.js
@@ -1,6 +1,24 @@
 const createCharacterBtn = $("#create-character-btn");
 const selectCharacterBtn = $("#select-character-btn");
+const classSelectBtn = document.querySelectorAll(".class-select-btn");
+let playbook;
 
 createCharacterBtn.click(function(){
     $("#class-selection-container").removeClass("hidden");
+})
+
+$("#class-card-container").click(function(event){
+
+    let element = event.target;
+
+    if (element.matches(".class-select-btn")) {
+        $("#class-selection-container").addClass("hidden");
+        $("#background-selector-container").removeClass("hidden");
+        playbook = element.getAttribute("data-playbook");
+    }
+
+    if (playbook === "blessed") {
+        $("#background-name").text("Initiate");
+        $("#background-description").html("<p>Stonetop has long been home to a sacred order, keepers of the old ways and speakers for Danu. You are one such initiate, the most gifted in generations.You gain the Rites of the Land move.</p><p>There are other initiates in Stonetop, serving the goddess and the village. They aid you as followers—see the Initiates of Danu insert. Who are they? Choose 2 or 3:</p><ul><li><b>Enfys</b>, your acolyte, beloved by birds</li><li><b>Afon</b>, strange and fae-touched</li><li><b>Gwendyl</b>, your mentor, a talented healer</li><li><b>Olwin</b>, your anointed lover, seer of fates</li><li><b>Seren the Eldest</b>, wise and hard as winter</li></ul>");
+    }
 })
